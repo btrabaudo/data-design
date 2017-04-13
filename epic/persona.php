954ab9a4ffd9b0c4a08e733a06e2c1b78b32f4c4
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-13 09:03:11 -0600

    Edited Persona. Added primary content to Use Case.

--- epic/persona.php
+++ epic/persona.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Data Design</title>
+        <!--h1 is used for assignment title only. This is the persona for a user who wants to "favorite a product"  -->
     </head>
 
     <body>
@@ -17,11 +18,11 @@
 
             <p><strong>Work Life:</strong> Jesse, is a sales executive at a paper supply company. He has an MBA from a mid tier college. After college he followed a traditional path through an internship and landed his first job. He has had one promotion and works long hours in a high demand industry.</p>
 
-            <p><strong>Personal Life:</strong>Do to his high intensity job and the long hours he works, Jesse is often looking for ways to release stress when away from work. As a college student he was on the swim team and generally athletic. This includes venturing into various low intensity athletic activities and also extensive partying with his peers on the weekend.</p>
+            <p><strong>Personal Life:</strong> Do to his high intensity job and the long hours he works, Jesse is often looking for ways to release stress when away from work. As a college student he was on the swim team and generally athletic. This includes venturing into various low intensity athletic activities and also extensive partying with his peers on the weekend. He is interested in the approval of his friends and likes being able to share his opinion on the products that interest him.</p>
 
-            <p><strong>Needs:</strong>Jesse, feels the pressure to advance at work but too often does not use the weekend to recharge. In order to advance at work Jesse must find a way to release stress and detox.</p>
+            <p><strong>Needs:</strong> Jesse, feels the pressure to advance at work but too often does not use the weekend to recharge. In order to advance at work Jesse must find a way to release stress and detox.</p>
 
-            <p><strong>Persona Conclusions:</strong>Because Jesse has a history of athletics but is also in need of a healthy detox, he is prime target for gym membership, a hot yoga class, and products geared to those options</p>
+            <p><strong>Persona Conclusions:</strong> Because Jesse has a history of athletics but is also in need of a healthy detox, he is prime target for gym membership, a hot yoga class, and products geared to those options. This will improve his weekends in themselves, his overall health will increase, which should lead to greater productivity at work, leading to advancement in his industry.</p>
 
 
         </main>
--- epic/use-case.php
+++ epic/use-case.php
@@ -11,6 +11,18 @@
         </header>
 
         <main>
+            <h2>Jesse Smith Use Case</h2>
+
+            <p><strong>Overview:</strong> Jesse, after receiving targeted marketing based on his persona will begin to access our site and explore targeted products in the health and wellness section.</p>
+
+            <p><strong>When:</strong>At work, the company restricts navigation to our site. Therefore he will be accessing our site primarily in the evening or on break.</p>
+
+            <p><strong>Utilized Tech:</strong> As a result of the above access time restrictions, the primary utilized tech will take two forms: smart phone and personal computer. His persona age indicates that he is likely to be using a laptop rather than a desktop. Further more he is likely to be using Chrome, Firefox, or Safari as his primary browser. He is a mainstream user so we should expect a OSX or Windows platform.</p>
+
+            <p><strong>Conclusions:</strong> Jesse will need a site that understands his needs and can effectively target him. Based on his persona he needs a site that allows him to efficiently purchase and rate products.</p>
+
+
+
 
 
         </main>
