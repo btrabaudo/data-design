23ad8612313621c36bdbbbaa551dfeb3e51b6c57
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-13 09:30:47 -0600

    Added primary conceptual model content

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -10,8 +10,38 @@
         </header>
 
         <main>
+            <ol>
+                <li><strong>Entities</strong></li>
+                    <ol>
+                        <li>Profile</li>
+                            <ul>
+                                <li><strong>Attributes</strong></li>
+                                    <ul>
+                                        <li>profileId</li>
+                                        <li>profileAtHandle</li>
+                                        <li>profileEmail</li>
+                                    </ul>
+                            </ul>
+                        <li>Product</li>
+                            <ul>
+                                <li><strong>Attributes</strong></li>
+                                    <ul>
+                                        <li>productId</li>
+                                        <li>productContent</li>
+                                        <li>productProfileId</li>
 
+                                    </ul>
+                            </ul>
+                        <li>Favorite Product (Weak)</li>
+                            <ul>
+                                <li><strong>Attributes</strong></li>
+                                    <ul>
+                                        <li>favoriteProductId</li>
+                                        <li>favoriteProfileId</li>
+                                    </ul>
 
+                            </ul>
+                    </ol>
         </main>
     </body>
 </html>
