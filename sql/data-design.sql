abc3eb7ec714011555222a7db9a2d4dc85bc760f
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-13 17:00:00 -0600

    Started SQL code

---
+++ sql/data-design.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS favorite;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile (
+
+profileID INT UNSIGNED AUTO_INCREMENT NOT NULL,
+profileActivationToken CHAR(32),
+profileAtHandle VARCHAR(32) NOT NULL,
+profileEmail VARCHAR(128) UNIQUE NOT NULL,
+profilePassHash CHAR (128) NOT NULL,
+profilePassSalt CHAR (64) NOT NULL,
+UNIQUE(profileEmail),
+UNIQUE(profileAtHandle)
+PRIMARY KEY(profileId)
+);
+
+CREATE TABLE product (
+
+productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+productContent VARCHAR (128) NOT NULL,
+productPrice DECIMAL NOT NULL,
+productDate DATETIME NOT NULL,
+
+)
