d93c756a445aa6158e2561f2c0b15e0873d007fd
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-13 08:31:13 -0600

    Primary content added to Persona

--- epic/persona.php
+++ epic/persona.php
@@ -11,7 +11,19 @@
         </header>
 
         <main>
-            <h2>Insert Persona Name</h2>
+            <h2>Jesse Smith</h2>
+
+            <p><strong>Age:</strong>30</p>
+
+            <p><strong>Work Life:</strong> Jesse, is a sales executive at a paper supply company. He has an MBA from a mid tier college. After college he followed a traditional path through an internship and landed his first job. He has had one promotion and works long hours in a high demand industry.</p>
+
+            <p><strong>Personal Life:</strong>Do to his high intensity job and the long hours he works, Jesse is often looking for ways to release stress when away from work. As a college student he was on the swim team and generally athletic. This includes venturing into various low intensity athletic activities and also extensive partying with his peers on the weekend.</p>
+
+            <p><strong>Needs:</strong>Jesse, feels the pressure to advance at work but too often does not use the weekend to recharge. In order to advance at work Jesse must find a way to release stress and detox.</p>
+
+            <p><strong>Persona Conclusions:</strong>Because Jesse has a history of athletics but is also in need of a healthy detox, he is prime target for gym membership, a hot yoga class, and products geared to those options</p>
+
+
         </main>
     </body>
 
