4227be19e2cec95afdf88e37d86b76ecaea2c02f
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-19 16:17:36 -0600

    Product functions without dateTime

--- php/Product.php
+++ php/Product.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Product Class for Data Design
  * author OED prop1@qed.net
  */
-
-class Product {
+class Product
+{
     /**
      * Id for product Id
      * @var int $productId
@@ -24,12 +25,12 @@ class Product {
     private $productDate;
 
 
-
     /**
      * accessor for product id
      * @return int|null $productId
      */
-    public function getProductId(): ?int {
+    public function getProductId(): ?int
+    {
         return ($this->productId);
     }
 
@@ -40,7 +41,8 @@ class Product {
      * @throws \InvalidArgumentException if product id is not an integer
      */
 
-    public function setProductId(int $newProductId) : void {
+    public function setProductId(int $newProductId): void
+    {
         //enforce that product id is not null
         if ($newProductId === null) {
             $this->productId = null;
@@ -48,7 +50,7 @@ class Product {
 
         }
         //enforce that product id is a positive int
-        if ($newProductId <=0) {
+        if ($newProductId <= 0) {
             throw(new \RangeException("product id is not positive"));
         }
         //store this product id
@@ -60,7 +62,8 @@ class Product {
      * accessor for product content
      * @return string for product content
      */
-    public function getProductContent(): string {
+    public function getProductContent(): string
+    {
         return $this->productContent;
     }
 
@@ -70,30 +73,47 @@ class Product {
      * @throws \InvalidArgumentException if content is not alphanumeric
      * @throws \RangeException if product content is more than 128 characters
      */
-    public function setProductContent(string $newProductContent) : void {
-        //enforce that product content is not null
-        if ($newProductContent === null) {
-            $this->productContent = null;
-            return;
-        }
+    public function setProductContent(string $newProductContent): void
+    {
+
+
         //enforce alphanumeric in product content
-        if (!ctype_alnum($newProductContent)){
+        if (!ctype_alnum($newProductContent)) {
             throw(new \InvalidArgumentException("product content must be alphanumeric"));
         }
+
         //enforce 128 characters in product content
         if (strlen($newProductContent) !== 128) {
             throw(new RangeException("product content must be 128 characters"));
         }
-    /**
-     * accessor for product price
-     * @return
-     */
-
-
-
     }
 
+    /**
+     * accessor for product price
+     * @return float for product price
+     */
+    public
+    function getProductPrice(): float
+    {
+        return $this->productPrice;
+    }
 
+    /**
+     * mutator for product price
+     * @param float $newProductPrice
+     * @throw \RangeException if content is not positive
+     */
+    public function setProductPrice(float $newProductPrice): void
+    {
+        //enforce product price as a positive integer
+        if ($newProductPrice < 0) {
+            throw(new \InvalidArgumentException("product price must be positive"));
+        }
+
+        //store product price
+        $this->productPrice = $newProductPrice;
+    }
+    /** Datetime function goes here */
 
 
 }
