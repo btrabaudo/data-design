5deff00fa03935acf6a4bb52f040756b3f0aaf6b
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-20 13:56:37 -0600

    Added primary content to date.php

--- php/date.php
+++ php/date.php
@@ -7,10 +7,31 @@ namespace Edu\Cnm\DataDesign;
  * Thanks to Dylan McDonald
  */
 
-trait ValidateDate {
+trait validateDate {
     /**
      * custom filter for mySQL date
      *
      */
+    private static function validateDate($newDate) : \DateTime {
+        if(is_object($newDate) === true && get_class($newDate) === "DateTime") {
+            return ($newDate);
+        }
+        //treat the date as a mySQL date string: Y-m-d
+        //p is pearl provided by Dylan
+        $newDate = trim($newDate);
+        if((preg_match("/^(\d{4})-(\d{2})-(\d{2})$/", $newDate, $matches)) !== 1) {
+            throw(new \InvalidArgumentException("date is not a valid date"));
+        }
+        // verify the date is really a valid calender date
+        $year = intval($matches[1]);
+        $month = intval($matches [2]);
+        $day = intval($matches [3]);
+        if (checkdate($month, $day, $year) === false) {
+            throw(new \RangeException("date is not a Gregorian date"));
+        }
+        //The date is clean.
+        $newDate = \DateTime::createFromFormat("Y-m-d H:i:s", $newDate . "00:00:00");
+        return($newDate);
+    }
 
 }
